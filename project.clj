ad170c4b297f767fc567f755fd306c3f7acaa6d3
Andrew Jackson <andrewsyoga@gmail.com>
2018-01-12 13:16:02 +0000

    Adding hiccup

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [compojure "1.6.0"]
-                 [http-kit "2.2.0"]])
+                 [http-kit "2.2.0"]
+                 [hiccup "1.0.5"]])
--- src/compojure_blog/core.clj
+++ src/compojure_blog/core.clj
@@ -1,10 +1,18 @@
 (ns compojure-blog.core
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [org.httpkit.server :refer [run-server]]))
+            [org.httpkit.server :refer [run-server]]
+            [hiccup.core :as hiccup]))
 
 (defroutes app
-  (GET "/" [] "Hello World")
+  (GET "/" []
+    (hiccup/html
+      [:html
+        [:head
+          [:title "My blog running on Clojure!"]]
+        [:body
+          [:h1
+            (str "Welcome!")]]]))
   (route/not-found "Page not found"))
 
 (defn -main []
