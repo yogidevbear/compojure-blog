37ec6cac095e15d9de5bb142b0c4c5d8dfa75964
Andrew Jackson <andrewsyoga@gmail.com>
2018-01-12 17:01:21 +0000

    Adding clojure.java.io to slurp in a temporary edn file with some fake blog data

--- src/compojure_blog/core.clj
+++ src/compojure_blog/core.clj
@@ -2,17 +2,24 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [org.httpkit.server :refer [run-server]]
+            [clojure.java.io :as io]
             [hiccup.core :as hiccup]))
+
+(defn slurp-file [filename]
+  (slurp (io/resource filename)))
 
 (defroutes app
   (GET "/" []
-    (hiccup/html
-      [:html
-        [:head
-          [:title "My blog running on Clojure!"]]
-        [:body
-          [:h1
-            (str "Welcome!")]]]))
+    (let [data (slurp-file "../resources/data.edn")]
+      data))
+  ;(GET "/" []
+;    (hiccup/html
+;      [:html
+;        [:head
+;          [:title "My blog running on Clojure!"]]
+;        [:body
+;          [:h1
+;            (str "Welcome!")]]]))
   (route/not-found "Page not found"))
 
 (defn -main []
