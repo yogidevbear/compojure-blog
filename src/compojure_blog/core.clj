2af186426a97b44f38145ae4d37db6d2f7bfa1a2
Andrew Jackson <Andrew Jackson>
2018-01-12 13:00:13 +0000

    Requiring compojure.route and adding a default 404 route

--- src/compojure_blog/core.clj
+++ src/compojure_blog/core.clj
@@ -1,9 +1,11 @@
 (ns compojure-blog.core
   (:require [compojure.core :refer :all]
+            [compojure.route :as route]
             [org.httpkit.server :refer [run-server]]))
 
 (defroutes app
-  (GET "/" [] "Hello World"))
+  (GET "/" [] "Hello World")
+  (route/not-found "Page not found"))
 
 (defn -main []
   (run-server app {:port 12345}))
