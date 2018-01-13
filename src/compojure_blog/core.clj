e32167c728c182cb325e92a9f2a8d9bf0986b780
Andrew Jackson <andrewsyoga@gmail.com>
2018-01-13 23:31:21 +0000

    Adding clojure.edn and updating hiccup for default route to try and output the categories from the data.edn file

--- src/compojure_blog/core.clj
+++ src/compojure_blog/core.clj
@@ -3,6 +3,7 @@
             [compojure.route :as route]
             [org.httpkit.server :refer [run-server]]
             [clojure.java.io :as io]
+            [clojure.edn :as edn]
             [hiccup.core :as hiccup]))
 
 (defn slurp-file [filename]
@@ -11,15 +12,18 @@
 (defroutes app
   (GET "/" []
     (let [data (slurp-file "../resources/data.edn")]
-      data))
-  ;(GET "/" []
-;    (hiccup/html
-;      [:html
-;        [:head
-;          [:title "My blog running on Clojure!"]]
-;        [:body
-;          [:h1
-;            (str "Welcome!")]]]))
+      ;(select-keys (edn/read-string data) [:categories]))
+      ;(select-keys (edn/read-string data) [:authors]))
+      (hiccup/html
+        [:html
+          [:head
+            [:title "My blog running on Clojure!"]]
+          [:body
+            [:h1 "Welcome"]
+            [:p (str (select-keys (edn/read-string data) [:categories]))]
+            [:ul
+              (for [category (select-keys (edn/read-string data) [:categories])]
+                [:li category])]]])))
   (route/not-found "Page not found"))
 
 (defn -main []
